4ecdc2624e43974ea5dc8ffc89672bcb27709e37
Veselin Ivanov <ivanov1234159@abv.bg>
2020-03-27 22:36:06 +0300

    declare functions (in Time.hpp) to class Time

--- Time.hpp
+++ Time.hpp
@@ -5,8 +5,33 @@
 #ifndef CALENDAR_TIME_HPP
 #define CALENDAR_TIME_HPP
 
-class Time {
+#include <iostream>
 
+class Time {
+    unsigned m_hours;
+    unsigned m_minutes;
+    unsigned m_seconds;
+public:
+    Time(unsigned hours = 0, unsigned minutes = 0, unsigned seconds = 0);
+    Time(std::istream& in);
+
+    unsigned getTotal() const;
+
+    int difference(Time const& other) const;
+
+    bool operator==(Time const& other) const;
+    bool operator>(Time const& other) const;
+    bool operator<(Time const& other) const;
+    bool operator>=(Time const& other) const;
+    bool operator<=(Time const& other) const;
+
+    operator int() const;
+    void print(std::ostream& out);
+
+private:
+    void normalize(unsigned hours, unsigned minutes, unsigned seconds);
 };
+
+std::ostream& operator<<(std::ostream& out, Time const& obj);
 
 #endif //CALENDAR_TIME_HPP
