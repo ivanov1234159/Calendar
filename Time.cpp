e43de2341755081f931760e00119573b23fc1d5d
Veselin Ivanov <ivanov1234159@abv.bg>
2020-03-27 21:56:25 +0300

    create class Time

---
+++ Time.cpp
@@ -0,0 +1,5 @@
+//
+// Created by toi on 27/03/2020.
+//
+
+#include "Time.hpp"
---
+++ Time.hpp
@@ -0,0 +1,12 @@
+//
+// Created by toi on 27/03/2020.
+//
+
+#ifndef CALENDAR_TIME_HPP
+#define CALENDAR_TIME_HPP
+
+class Time {
+
+};
+
+#endif //CALENDAR_TIME_HPP
