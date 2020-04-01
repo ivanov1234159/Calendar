1114d653bf2321b9f7b05ee5e4a61384798260dc
Veselin Ivanov <ivanov1234159@abv.bg>
2020-04-01 22:51:05 +0300

    create open(), close(), clear() and opened() methods in class Program; create constructor and destructor of class Program; create private field m_calendar from type Calendar; disable copy constructor and operator=

--- Program.cpp
+++ Program.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "Program.hpp"
+
+Program::Program(): m_calendar(nullptr) {}
+
+Program::~Program() {
+    clear();
+}
+
+bool Program::opened() const {
+    return m_calendar != nullptr;
+}
+
+bool Program::open(char const *file_path) {
+    if(opened()){
+        return false;
+    }
+    m_calendar = new Calendar(file_path);
+    return true;
+}
+
+bool Program::close(char *&file_path) {
+    if(!opened()){
+        return true;
+    }
+    if(file_path != nullptr){
+        return false;
+    }
+    m_calendar->getName(file_path);
+    clear();
+    return true;
+}
+
+void Program::clear() {
+    if(m_calendar != nullptr){
+        delete m_calendar;
+        m_calendar = nullptr;
+    }
+}
--- Program.hpp
+++ Program.hpp
@@ -5,8 +5,24 @@
 #ifndef CALENDAR_PROGRAM_HPP
 #define CALENDAR_PROGRAM_HPP
 
-class Program {
+#include "Calendar.hpp"
 
+class Program {
+    Calendar* m_calendar;// only one; (not a list)
+
+    Program(Program const& other) = delete;
+    Program& operator=(Program const& other) = delete;
+public:
+    Program();
+    ~Program();
+
+    bool opened() const;
+
+    bool open(char const* file_path);
+    bool close(char*& file_path);// file_path is to return the file_path (from m_calendar->m_name)
+
+private:
+    void clear();
 };
 
 #endif //CALENDAR_PROGRAM_HPP
