e041c7fb7b2abf5253a44d93504f31087aca9a1a
Veselin Ivanov <ivanov1234159@gmail.com>
2020-06-02 18:53:45 +0300

    create class CmdBusydays

---
+++ cmd/CmdBusydays.hpp
@@ -0,0 +1,16 @@
+//
+// Created by toi on 29/05/2020.
+//
+
+#ifndef CALENDAR_CMDBUSYDAYS_HPP
+#define CALENDAR_CMDBUSYDAYS_HPP
+
+#include "../Command.hpp"
+
+class CmdBusydays : public Command {
+public:
+    CmdBusydays();
+    virtual bool action(RunnerType& runner, std::istringstream& iss) const override;
+};
+
+#endif //CALENDAR_CMDBUSYDAYS_HPP
