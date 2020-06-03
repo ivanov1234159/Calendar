94c1251eacc2ceed7efc6954f8f4812aa16ca03b
Veselin Ivanov <ivanov1234159@gmail.com>
2020-06-03 15:01:15 +0300

    add the new commands in build_commands.cpp

--- build_commands.cpp
+++ build_commands.cpp
@@ -3,42 +3,38 @@
 //
 
 #include "Commander.hpp"
+#include "CmdAgenda.hpp"
 #include "CmdBook.hpp"
+#include "CmdBusydays.hpp"
+#include "CmdChange.hpp"
 #include "CmdClose.hpp"
 #include "CmdExit.hpp"
+#include "CmdFind.hpp"
+#include "CmdFindSlot.hpp"
+#include "CmdFindSlotWith.hpp"
 #include "CmdHelp.hpp"
+#include "CmdHoliday.hpp"
 #include "CmdOpen.hpp"
 #include "CmdSave.hpp"
 #include "CmdSaveAs.hpp"
 #include "CmdUnbook.hpp"
 
-bool cmd_status(RunnerType& runner, std::istringstream& iss){
-    std::cout << "File opened: " << (runner.opened() ? "true" : "false") << std::endl;
-    std::cout << "File path: " << (runner.opened()
-        ? (runner.m_calendar->getFilePath() == nullptr
-            ? "<NULL>"
-            : runner.m_calendar->getFilePath())
-        : "<NO_PATH>") << std::endl;
-    std::cout << "Appointments: " << std::endl << std::endl;
-    if(runner.opened()){
-        for(unsigned i = 0; i < runner.m_calendar->m_size; i++){
-            std::cout << runner.m_calendar->m_list[i] << std::endl;
-        }
-        std::cout << std::endl;
-    }else{
-        std::cout << "No appointments." << std::endl;
-    }
-    return true;
-}
-
 void build_commands(){
+    Commander::add(new CmdAgenda);
     Commander::add(new CmdBook);
+    Commander::add(new CmdBusydays);
+    Commander::add(new CmdChange);
     Commander::add(new CmdClose);
     Commander::add(new CmdExit);
+    Commander::add(new CmdFind);
+    Commander::add(new CmdFindSlot);
+    Commander::add(new CmdFindSlotWith);
     Commander::add(new CmdHelp);
+    Commander::add(new CmdHoliday);
     Commander::add(new CmdOpen);
     Commander::add(new CmdSave);
     Commander::add(new CmdSaveAs);
     Commander::add(new CmdUnbook);
-    /*Commander::add(Command("status", "", "prints information about the program status", cmd_status));*/
 }
+
+
