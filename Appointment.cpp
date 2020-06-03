24c11dd586246f4fbdb332b29e979c33c176ff14
Veselin Ivanov <ivanov1234159@gmail.com>
2020-06-03 19:28:31 +0300

    IGNORE this commit; minor changes on Appointment::getStartTime() and Appointment::getEndTime()

--- Appointment.cpp
+++ Appointment.cpp
@@ -34,13 +34,13 @@ String const& Appointment::getNote() const  { return m_note;    }
 Date Appointment::getDate() const           { return m_date;    }
 Time Appointment::getStartTime(int diff_seconds) const {
     if(diff_seconds != 0){
-        return Time(diff_seconds + m_start.getTotal());
+        return Time(diff_seconds + (int)m_start.getTotal());
     }
     return m_start;
 }
 Time Appointment::getEndTime(int diff_seconds) const {
     if(diff_seconds != 0){
-        return Time(diff_seconds + m_end.getTotal());
+        return Time(diff_seconds + (int)m_end.getTotal());
     }
     return m_end;
 }
